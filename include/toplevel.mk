wl500g/toolchain

commit 926436408bb96a7b81cf2af66dac17906897384b

toolchain: toplevel.mk: OpenWRT's changesets 11475, 12254, 14737, 14740
 add another check to the dependencies
 fix make distclean
 avoid implicit rules in even more places
 optimize the .config dependency chain in the toplevel makefile


git-svn-id: https://wl500g.googlecode.com/svn/toolchain@5564 c3c545f6-d8d2-11dd-93da-dbb4cac96d7c

include/toplevel.mk
@@ -32,8 +32,8 @@ prepare-mk: FORCE ;
 
 prepare-tmpinfo: FORCE
 	mkdir -p tmp/info
-#	$(_SINGLE)$(NO_TRACE_MAKE) -j1 -s -f include/scan.mk SCAN_TARGET="packageinfo" SCAN_DIR="package" SCAN_NAME="package" SCAN_DEPS="$(TOPDIR)/include/package*.mk" SCAN_DEPTH=5 SCAN_EXTRA=""
-#	$(_SINGLE)$(NO_TRACE_MAKE) -j1 -s -f include/scan.mk SCAN_TARGET="targetinfo" SCAN_DIR="target/linux" SCAN_NAME="target" SCAN_DEPS="profiles/*.mk $(TOPDIR)/include/kernel*.mk $(TOPDIR)/include/target.mk" SCAN_DEPTH=2 SCAN_EXTRA="" SCAN_MAKEOPTS="TARGET_BUILD=1"
+#	$(_SINGLE)$(NO_TRACE_MAKE) -j1 -r -s -f include/scan.mk SCAN_TARGET="packageinfo" SCAN_DIR="package" SCAN_NAME="package" SCAN_DEPS="$(TOPDIR)/include/package*.mk" SCAN_DEPTH=5 SCAN_EXTRA=""
+#	$(_SINGLE)$(NO_TRACE_MAKE) -j1 -r -s -f include/scan.mk SCAN_TARGET="targetinfo" SCAN_DIR="target/linux" SCAN_NAME="target" SCAN_DEPS="profiles/*.mk $(TOPDIR)/include/kernel*.mk $(TOPDIR)/include/target.mk" SCAN_DEPTH=2 SCAN_EXTRA="" SCAN_MAKEOPTS="TARGET_BUILD=1"
 #	for type in package target; do \
 #		f=tmp/.$${type}info; t=tmp/.config-$${type}.in; \
 #		[ "$$t" -nt "$$f" ] || ./scripts/metadata.pl $${type}_config "$$f" > "$$t" || { rm -f "$$t"; echo "Failed to build $$t"; false; break; }; \
@@ -41,7 +41,7 @@ prepare-tmpinfo: FORCE
 #	./scripts/metadata.pl package_mk tmp/.packageinfo > tmp/.packagedeps || { rm -f tmp/.packagedeps; false; }
 	touch $(TOPDIR)/tmp/.build
 
-.config: ./scripts/config/conf prepare-tmpinfo
+.config: ./scripts/config/conf $(if $(CONFIG_HAVE_DOT_CONFIG),,prepare-tmpinfo)
 	@+if [ \! -f .config ]; then \
 		[ -e defconfig ] && cp defconfig .config; \
 		$(NO_TRACE_MAKE) menuconfig $(PREP_MK); \
@@ -83,7 +83,7 @@ kernel_menuconfig: .config FORCE
 tmp/.prereq-build: include/prereq-build.mk
 	mkdir -p tmp
 	rm -f tmp/.host.mk
-	@$(_SINGLE)$(NO_TRACE_MAKE) -j1 -s -f $(TOPDIR)/include/prereq-build.mk prereq 2>/dev/null || { \
+	@$(_SINGLE)$(NO_TRACE_MAKE) -j1 -r -s -f $(TOPDIR)/include/prereq-build.mk prereq 2>/dev/null || { \
 		echo "Prerequisite check failed. Use CHECK_<tool>=0 to override."; \
 		false; \
 	}
@@ -94,14 +94,14 @@ download: .config FORCE
 	@+$(SUBMAKE) toolchain/download
 
 clean dirclean: .config
-	@+$(SUBMAKE) $@ 
+	@+$(SUBMAKE) -r $@ 
 
-prereq:: .config
-	@+$(MAKE) -s tmp/.prereq-build $(PREP_MK)
-	@$(NO_TRACE_MAKE) -s $@
+prereq:: prepare-tmpinfo .config
+	@+$(MAKE) -r -s tmp/.prereq-build $(PREP_MK)
+	@+$(NO_TRACE_MAKE) -r -s $@
 
 %::
-	@+$(PREP_MK) $(NO_TRACE_MAKE) -s prereq
+	@+$(PREP_MK) $(NO_TRACE_MAKE) -r -s prereq
 	@+$(SUBMAKE) -r $@
 
 help:
@@ -115,7 +115,7 @@ docs/clean: FORCE
 
 distclean:
 	rm -rf tmp build_dir staging_dir dl .config* feeds bin
-	@$(_SINGLE)$(SUBMAKE) -j1 -C scripts/config clean
+	@$(SUBMAKE) -j1 -C scripts/config clean
 
 ifeq ($(findstring v,$(DEBUG)),)
   .SILENT: symlinkclean clean dirclean distclean config-clean download help tmpinfo-clean .config scripts/config/mconf scripts/config/conf menuconfig tmp/.prereq-build tmp/.prereq-package prepare-tmpinfo

commit 243ee93c75ba32f28c344b38d8be3a3dad1d4d33

toolchain: toplevel.mk: Fix breakage introduced in r5564
git-svn-id: https://wl500g.googlecode.com/svn/toolchain@5572 c3c545f6-d8d2-11dd-93da-dbb4cac96d7c

include/toplevel.mk
@@ -42,7 +42,7 @@ prepare-tmpinfo: FORCE
 	touch $(TOPDIR)/tmp/.build
 
 .config: ./scripts/config/conf $(if $(CONFIG_HAVE_DOT_CONFIG),,prepare-tmpinfo)
-	@+if [ -f .config ] || ! grep CONFIG_HAVE_DOT_CONFIG .config >/dev/null; then \
+	@+if [ \! -f .config ] || ! grep CONFIG_HAVE_DOT_CONFIG .config >/dev/null; then \
 		[ -e defconfig ] && cp defconfig .config; \
 		$(NO_TRACE_MAKE) menuconfig $(PREP_MK); \
 	fi
@@ -101,7 +101,7 @@ prereq:: prepare-tmpinfo .config
 	@+$(NO_TRACE_MAKE) -r -s $@
 
 %::
-	@+$(PREP_MK) $(NO_TRACE_MAKE) -r -s prereq
+	@+$(PREP_MK) $(NO_TRACE_MAKE) -s prereq
 	@+$(SUBMAKE) -r $@
 
 help: